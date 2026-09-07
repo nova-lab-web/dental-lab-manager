76318286bc032ec1e5a13c98ceb89e3ff196bd88
nova-lab-web <bodest1987ful@gmail.com>
2026-09-07 11:31:46 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'nova-dental-v1';
+const APP_SHELL = self.location.pathname.replace(/sw\.js$/, 'Nova-Dental-Lab-fixed-v15.html');
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll([APP_SHELL]))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match(APP_SHELL)))
+  );
+});
